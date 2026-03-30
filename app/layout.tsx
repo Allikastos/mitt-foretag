654d4189a0354b563dd886f00d6c0c07a8675968
Albin Holmberg <albinholmberg@MacBook-Air-som-tillhor-Albin-2.local>
2026-03-30 14:20:29 +0200

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,7 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sv" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VH5R30NZY6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VH5R30NZY6');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
